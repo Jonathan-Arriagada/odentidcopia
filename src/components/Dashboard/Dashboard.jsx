afa4123298d77ca39a1fe39348f010ce501e5170
FSArriagada <facu._99@hotmail.com>
2023-06-20 18:53:31 -0300

    React Chartjs

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,13 +2,33 @@ import React, { useCallback, useContext, useState } from 'react'
 import Navigation from '../Navigation'
 import Swal from 'sweetalert2';
 import { Link, useNavigate } from 'react-router-dom';
-import { FaSignOutAlt, FaBell } from "react-icons/fa";
+import { FaSignOutAlt, FaBell, FaBold } from "react-icons/fa";
 import profile from "../../img/profile.png";
 import "../../style/Main.css";
 import { AuthContext } from '../../context/AuthContext';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
 
 function Dashboard() {
-  
+   
 const [search, setSearch] = useState("");
 const [isLoading, setIsLoading] = useState(true);
 
@@ -38,6 +58,57 @@ const logout = useCallback(() => {
 
   const searcher = (e) => {
     setSearch(e.target.value);
+  };
+
+  const data = {
+    labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    datasets: [{
+      data: [9, 14, 20, 12, 32, 15, 39, 27, 11, 18, 25, 22],
+      backgroundColor: '#47abff',
+    }]
+  };
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Tratamientos por mes',
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+        font: {
+          weight: 'bold',
+          size: 24,
+          family: 'Goldplay'
+        },
+        color: '#FFF',
+      }
+    },
+    scales: {
+      y: {
+        min: 5,
+        max: 60,
+        ticks: {
+          stepSize: 10,
+          color: '#FFF',
+        },
+        grid: {
+          borderDash: [8],
+          color: '#2e3e62',
+        }
+      },
+      x: {
+        ticks: {
+          color: '#FFF',
+        },
+        grid: {
+          color: 'transparent',
+        },
+      },
+    },
   };
 
   return (
@@ -87,9 +158,49 @@ const logout = useCallback(() => {
             </div>
           </div>
         </nav>
-        </div >
+        <div className='container mt-2'>
+          <div className="row m-2 flex-nowrap" style={{width: "70vw"}}>
+            <div className="col-7 pb-3 me-2 rounded-4 shadow fondo-color-primario" style={{ height: '40vh' }}>
+              <Bar data={data} options={options}></Bar>
+            </div>
+            <div className="col-5 ms-2 rounded-4 d-flex justify-content-center align-items-start flex-column shadow" style={{ height: '40vh' }}>
+              <h2 className="fw-bold fs-6">Productividad de los dentistas</h2>
+              <h3 className="fs-1">78%</h3>
+              <h2 className="fw-bold fs-6">Eficiencia en el proceso de facturacion</h2>
+              <h3 className="fs-1">48%</h3>
+            </div>
+          </div>
+          <div className="row m-2 mt-4 flex-nowrap " style={{width: "70vw"}}>
+          <div className="col-3 me-1 rounded-4 d-flex justify-content-center align-items-start flex-column shadow" style={{ height: '40vh' }}>
+            <h2 className="fw-bold fs-6">Pacientes nuevos</h2>
+            <h3 className="fs-1">4</h3>
+            <h2 className="fw-bold fs-6">Pacientes atendidos</h2>
+            <h3 className="fs-1">21</h3>
+            <h2 className="fw-bold fs-6">Nuevos casos de ortodoncia</h2>
+            <h3 className="fs-1">3</h3>
+          </div>
+          <div className="col-3 mx-1 rounded-4 d-flex justify-content-center align-items-start flex-column shadow" style={{ height: '40vh' }}>
+            <h2 className="fw-bold fs-6">Índice de satisfacción del paciente</h2>
+            <h3 className="fs-1">4.2</h3>
+            <h2 className="fw-bold fs-6">Índice de cancelación y ausencia de citas</h2>
+            <h3 className="fs-1">34%</h3>
+          </div>
+          <div className="col-3 mx-1 rounded-4 d-flex justify-content-center align-items-start flex-column shadow" style={{ height: '40vh' }}>
+            <h2 className="fw-bold fs-6">Ingresos y Rentabilidad</h2>
+            <h3 className="fs-1">4</h3>
+            <h2 className="fw-bold fs-6">Ingresos por tratamiento</h2>
+            <h3 className="fs-1">7</h3>
+          </div>
+          <div className="col-3 ms-1 rounded-4 d-flex justify-content-center align-items-start flex-column shadow" style={{ height: '40vh' }}>
+            <h2 className="fw-bold fs-6">Tasa de retención de pacientes</h2>
+            <h3 className="fs-1">78%</h3>
+          </div>
 
-  </div>
+          </div>
+        </div>
+     </div>
+    
+</div>
   );
 };
 
